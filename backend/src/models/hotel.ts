9dbe49791014703631d1c280a30ea9e9f6727108
Mujeeb02 <83205006+Mujeeb02@users.noreply.github.com>
2024-06-28 20:07:10 +0530

    fixed deployment

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import { BookingType } from "../../shared/types";
 
 export type HotelType = {
@@ -18,17 +18,6 @@ export type HotelType = {
     lastUpdated: Date;
   };
 
-  const bookingSchema = new mongoose.Schema<BookingType>({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    adultCount: { type: Number, required: true },
-    childCount: { type: Number, required: true },
-    checkIn: { type: Date, required: true },
-    checkOut: { type: Date, required: true },
-    userId: { type: String, required: true },
-    totalCost: { type: Number, required: true },
-  });
 
   const hotelSchema = new mongoose.Schema<HotelType>({
     userId: {
